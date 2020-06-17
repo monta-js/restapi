3ccdc1cf5ebb899e130449fb5e3daae437311303
monta-js <trichili.montassar@gmail.com>
2020-06-17 04:53:28 +0200

    last commit

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   axios.all([
-    axios.get('https://api.openweathermap.org/data/2.5/weather?q=Tunis&appid=371f668ca7a2429f03ec4e1032b75741'),
     axios.get('https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=371f668ca7a2429f03ec4e1032b75741'),
-    axios.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=371f668ca7a2429f03ec4e1032b75741'),
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=Oran&appid=371f668ca7a2429f03ec4e1032b75741'),
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=New york&appid=371f668ca7a2429f03ec4e1032b75741'),
 
   ])
   .then(responseArr => {
